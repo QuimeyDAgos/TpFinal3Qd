4ecd85f504218e93bedd426a8bfd85a41c9f70d8
lautaro13dejulio <lautaro13dejulio@gmail.com>
2024-06-15 17:17:17 -0300

    Cree dos excepciones y las implemente en productos vendible, usando estas dos excepciones

--- src/ProductosPack/Comida.java
+++ src/ProductosPack/Comida.java
@@ -1,6 +1,9 @@
 package ProductosPack;
 
-public class Comida extends Productos {
+import Excepciones.ProductoNoDisponibleExepcion;
+import Interfaces.Vendible;
+
+public class Comida extends Productos implements Vendible {
     private String tipo;
 
     public String getTipo() {
@@ -11,9 +14,37 @@ public class Comida extends Productos {
         this.tipo = tipo;
     }
 
-    public Comida(String nombre, double precio, int stock, String tipo) {
-        super(nombre, precio, stock);
+    public Comida(String nombre, double precio, int stock, String tipo,boolean disponibilidad) {
+        super(nombre, precio, stock,disponibilidad);
         this.tipo = tipo;
+    }
+
+@Override 
+    public void venta()
+    {
+        try{
+        if(stock>0 && disponibilad)
+        {
+           System.out.println("Se realizo la venta con exito");
+           stock--;
+           if(stock==0)
+           {
+            this.disponibilad=false;
+           }
+        }
+        else{
+            throw new ProductoNoDisponibleExepcion("El producto no esta disponible");
+        }
+        }catch(ProductoNoDisponibleExepcion e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void aumento(double porcentaje)
+    {  
+        setPrecio(precio+(precio*porcentaje));
     }
 
     @Override
